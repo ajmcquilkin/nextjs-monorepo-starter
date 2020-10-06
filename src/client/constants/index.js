6864b594cc57d54ad2958928a1f06022d46e0254
nathan-m-schneider-22 <nathan.m.schneider.22@dartmouth.edu>
2020-10-06 17:18:29 -0400

    deployment change

--- src/client/constants/index.js
+++ src/client/constants/index.js
@@ -2,7 +2,7 @@
 // * Eslint disabled because this variable is defined in "root/webpack.config.js"
 // eslint-disable-next-line no-undef
 // export const ROOT_URL = REACT_APP_ROOT_URL || 'localhost:9090';
-export const ROOT_URL = 'http://localhost/api';
+export const ROOT_URL = `${window.location.origin}/api`;
 
 // Auth token name for storage and transmission to backend
 export const authTokenName = 'authToken';
