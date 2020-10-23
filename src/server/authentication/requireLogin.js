417c6d11828339c2ddd669b913687a8be834350f
nathan-m-schneider-22 <nathan.m.schneider.22@dartmouth.edu>
2020-10-23 02:23:33 -0400

    diff CAS payload deployed

--- src/server/authentication/requireLogin.js
+++ src/server/authentication/requireLogin.js
@@ -1,6 +1,15 @@
 const requireLogin = (req, res, next) => {
   console.log(req.session);
   if (!req.session.info) return res.status(401).json({ message: 'Unauthorized' });
+
+  // default CAS return
+  if (req.session.info.netid) {
+    return next();
+  }
+
+  // deployed CAS return
+  req.session.info.netid = req.session.cas_user;
+  req.session.cas_user = req.session.info.email;
   return next();
 };
 
