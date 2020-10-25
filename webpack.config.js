8a75068b11a26d109bc373683886916c4991c384
rh516 <rrhuang99@gmail.com>
2020-10-24 21:49:52 -0400

    added national font

--- webpack.config.js
+++ webpack.config.js
@@ -51,7 +51,7 @@ module.exports = {
       ],
     },
     {
-      test: /\.(png|woff|woff2|eot|ttf|svg)$/,
+      test: /\.(png|woff|woff2|eot|ttf|otf|svg)$/,
       loader: 'url-loader?limit=100000'
     }
     ]
