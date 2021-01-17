1e718b3342c2bd346a18531a7b0da530591d1ec4
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2021-01-17 00:07:12 -0500

    Secured sensitive env values within webpack config

--- next.config.js
+++ next.config.js
@@ -3,12 +3,16 @@ module.exports = {
     buildId, dev, isServer, defaultLoaders, webpack
   }) => {
     config.plugins.push(new webpack.DefinePlugin({
-      __AUTH_SECRET__: JSON.stringify(process.env.AUTH_SECRET || ''),
-      __SESSION_SECRET__: JSON.stringify(process.env.SESSION_SECRET || ''),
-      __MONGODB_URI__: JSON.stringify(process.env.MONGODB_URI || ''),
+      ...(isServer ? {
+        // ! Secure values here
+        __AUTH_SECRET__: JSON.stringify(process.env.AUTH_SECRET || ''),
+        __SESSION_SECRET__: JSON.stringify(process.env.SESSION_SECRET || ''),
+        __MONGODB_URI__: JSON.stringify(process.env.MONGODB_URI || ''),
+        __SERVICE_URL__: JSON.stringify(process.env.SERVICE_URL || ''),
+      } : {}),
 
+      // !!! ONLY place INSECURE values here
       __MODE__: JSON.stringify(process.env.MODE || ''),
-      __SERVICE_URL__: JSON.stringify(process.env.SERVICE_URL || ''),
       __APP_URL__: JSON.stringify(process.env.APP_URL || ''),
     }));
 
