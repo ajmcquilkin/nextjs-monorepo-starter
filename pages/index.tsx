bafa92a77ee6c3358d2d29f0f8b9808998a3be3e
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2021-02-02 16:58:03 -0500

    Added a date-standardizing util function

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,14 @@ import { GetStaticProps } from 'next';
 
 import Home, { HomePassedProps } from 'components/pages/home';
 import { fetchReleaseByDateRequest } from 'store/requests/releaseRequests';
+
+import { getMidnightDate } from 'utils';
 import { FetchReleaseData } from 'types/release';
 
 export const getStaticProps: GetStaticProps<HomePassedProps> = async () => {
-  // TODO: make helper function to get correct date from passed number
-  const date = 0;
+  const currentDate = getMidnightDate(Date.now());
 
-  // TODO: refactor return types into request functions
-  const { data: { data: { posts, release } } } = await fetchReleaseByDateRequest<FetchReleaseData>(date)();
+  const { data: { data: { posts, release } } } = await fetchReleaseByDateRequest<FetchReleaseData>(currentDate)();
   const releasePostMap = posts.reduce((accum, post) => ({ ...accum, [post._id]: post }), {});
 
   return ({ props: { release, releasePostMap } });
