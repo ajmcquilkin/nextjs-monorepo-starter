252de89b9f815f57d2fcc98ba1dbaa8a3896529c
Satch Baker <56321655+satchb7@users.noreply.github.com>
2021-02-03 12:46:17 -0500

    adding HTML sanitize, filter method, and populating results

--- pages/api/posts/index.ts
+++ pages/api/posts/index.ts
@@ -4,13 +4,26 @@ import { createDefaultHandler, createSuccessPayload } from 'utils/api';
 import { casInstance } from 'utils/auth';
 import { useDB } from 'utils/db';
 
-import { FetchPostData, FetchPostsData } from 'types/post';
+import {
+  FetchPostData, FetchPostResultsData, FetchPostsData,
+  PostStatus
+} from 'types/post';
 
 const handler = createDefaultHandler()
   .use(useDB)
   .use(casInstance.bounce)
 
-  .get(async (_req, res) => {
+  .get(async (req, res) => {
+    const { status } = req.query;
+
+    if (status && typeof status === 'string') {
+      const posts = await postController.readAllByStatus(status as PostStatus);
+      const results = posts.map((post) => post._id);
+      const numResults = results.length;
+
+      return res.status(200).json(createSuccessPayload<FetchPostResultsData>({ posts, results, numResults }));
+    }
+
     const foundPosts = await postController.readAll();
     return res.status(200).json(createSuccessPayload<FetchPostsData>({ posts: foundPosts }));
   })
