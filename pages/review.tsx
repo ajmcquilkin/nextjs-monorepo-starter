f54f7d4c63e29b0f379e1e8b335a199003e4ca15
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2021-02-18 13:25:36 -0500

    Implemented skeleton loading on review page

--- pages/review.tsx
+++ pages/review.tsx
@@ -1,12 +1,17 @@
 import { connect } from 'react-redux';
 
 import Review, { ReviewStateProps, ReviewDispatchProps, ReviewPassedProps } from 'components/pages/review';
+
+import { createLoadingSelector } from 'store/actionCreators/requestActionCreators';
 import { fetchWithStatus } from 'store/actionCreators/postActionCreators';
 
 import { RootState } from 'types/state';
 
+const postLoadingSelector = createLoadingSelector(['FETCH_POST_RESULTS']);
+
 const mapStateToProps = (state: RootState): ReviewStateProps => ({
-  currentPosts: state.post.results.map((id) => state.post.posts[id])
+  currentPosts: state.post.results.map((id) => state.post.posts[id]),
+  isLoading: postLoadingSelector(state)
 });
 
 const mapDispatchToProps: ReviewDispatchProps = {
