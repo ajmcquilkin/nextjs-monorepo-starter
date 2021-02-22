88c57ecb25a5f8ce23e34e0adf33639c2ddd2027
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2021-02-22 18:10:29 -0500

    Styled review page

--- components/layout/filterBar/index.tsx
+++ components/layout/filterBar/index.tsx
@@ -5,42 +5,49 @@ export interface FilterBarProps {
   status: PostStatus | '',
   type: PostPublishType | '',
   setStatus: (status: PostStatus | '') => void
-  setType: (type: PostPublishType | '') => void
+  setType: (type: PostPublishType | '') => void,
+
+  hideStatus?: boolean,
+  hideType?: boolean
 }
 
 const FilterBar = ({
-  status, type, setStatus, setType
+  status, type, setStatus, setType, hideStatus = false, hideType = false
 }: FilterBarProps): JSX.Element => (
   <div className={styles.filterBarContainer}>
-    <div className={styles.selectContainer}>
-      <p>Status</p>
-      <select
-        name="status"
-        value={status}
-        onChange={(e) => setStatus((e.target.value) as (PostStatus | ''))}
-      >
-        <option value="">View All</option>
-        <option value="draft">Draft</option>
-        <option value="pending">Pending</option>
-        <option value="approved">Approved</option>
-        <option value="rejected">Rejected</option>
-        <option value="published">Published</option>
-      </select>
-    </div>
+    {!hideStatus && (
+      <div className={styles.selectContainer}>
+        <p>Status</p>
+        <select
+          name="status"
+          value={status}
+          onChange={(e) => setStatus((e.target.value) as (PostStatus | ''))}
+        >
+          <option value="">View All</option>
+          <option value="draft">Draft</option>
+          <option value="pending">Pending</option>
+          <option value="approved">Approved</option>
+          <option value="rejected">Rejected</option>
+          <option value="published">Published</option>
+        </select>
+      </div>
+    )}
 
-    <div className={styles.selectContainer}>
-      <p>Type</p>
-      <select
-        name="postType"
-        value={type}
-        onChange={(e) => setType((e.target.value) as (PostPublishType | ''))}
-      >
-        <option value="">View All</option>
-        <option value="news">News</option>
-        <option value="announcement">Announcement</option>
-        <option value="event">Event</option>
-      </select>
-    </div>
+    {!hideType && (
+      <div className={styles.selectContainer}>
+        <p>Type</p>
+        <select
+          name="postType"
+          value={type}
+          onChange={(e) => setType((e.target.value) as (PostPublishType | ''))}
+        >
+          <option value="">View All</option>
+          <option value="news">News</option>
+          <option value="announcement">Announcement</option>
+          <option value="event">Event</option>
+        </select>
+      </div>
+    )}
   </div>
 );
 
