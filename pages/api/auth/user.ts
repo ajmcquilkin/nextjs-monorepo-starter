fdbb101cd22504042cf002231783bdb79777bf1b
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2021-02-23 21:20:36 -0500

    Initial commit

--- pages/api/auth/user.ts
+++ pages/api/auth/user.ts
@@ -1,9 +1,12 @@
 import { createDefaultHandler, createSuccessPayload } from 'utils/api';
+import { devModeInfo } from 'utils/auth';
 
 import { AuthUserData } from 'types/user';
 
 const handler = createDefaultHandler<AuthUserData>({ requireAuth: false })
   .get((req, res) => {
+    if (__ENABLE_CAS_DEV_MODE__) return res.status(200).json(createSuccessPayload<AuthUserData>({ ...devModeInfo, isAuthenticated: true }));
+
     const isAuthenticated = !!req.session.casUser;
     const isStaff = req.session.info?.isStaff ?? false;
     const isReviewer = req.session.info?.isReviewer ?? false;
