c7a092636d8aa6b46b6ba22fa7cfa53fe7391886
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2021-02-27 23:11:32 -0500

    Styled review page

--- components/submissions/reviewSubmission/index.tsx
+++ components/submissions/reviewSubmission/index.tsx
@@ -1,6 +1,10 @@
 import Link from 'next/link';
 import sanitizeHtml from 'sanitize-html';
+
+import { getColorsForStatus, getFullDate, uppercaseFirstLetter } from 'utils';
 import { Post } from 'types/post';
+
+import styles from './reviewSubmission.module.scss';
 
 export interface ReviewSubmissionProps {
   content: Post,
@@ -12,31 +16,69 @@ const ReviewSubmission = ({ content, onApprove, onReject }: ReviewSubmissionProp
   const sanitizedHTML = sanitizeHtml(content.fullContent);
 
   return (
-    <div>
-      <div>
-        <div>
-          <h3>{content.briefContent}</h3>
-          <p>{content.eventDate}</p>
-        </div>
-
-        <div>
-          <p>{content.type}</p>
-          <p>{content.status}</p>
-        </div>
+    <div className={styles.reviewSubmissionContainer} style={{ borderLeftColor: getColorsForStatus(content.status).primary }}>
+      <div className={styles.titleContainer}>
+        <h3>{content.briefContent}</h3>
+        <p>
+          {getFullDate(content.requestedPublicationDate)}
+          {' '}
+          &bull;
+          {' '}
+          {uppercaseFirstLetter(content.type)}
+          {' '}
+          &bull;
+          {' '}
+          <span>{uppercaseFirstLetter(content.status)}</span>
+        </p>
       </div>
 
-      <div>
-        <p>{content.fromName}</p>
-        <p>{content.recipientGroups}</p>
+      {content.type === 'event' && <p className={styles.subtitle}>{getFullDate(content.eventDate ?? Date.now())}</p>}
+
+      <p className={styles.sendingInfo}>
+        {content.fromName}
+        {' '}
+        &bull;
+        {' '}
+        {content.recipientGroups.join(', ')}
+      </p>
+
+      <div className={styles.contentContainer}>
+        <div className={styles.content} dangerouslySetInnerHTML={{ __html: sanitizedHTML }} />
+        <Link href={content.url}><a className={styles.contentLink}>{content.url}</a></Link>
       </div>
 
-      <div dangerouslySetInnerHTML={{ __html: sanitizedHTML }} />
-      <Link href={content.url}><a>{content.url}</a></Link>
+      <div className={styles.buttonContainer}>
+        <button
+          className={styles.approve}
+          type="button"
+          onClick={() => onApprove(content._id)}
+        >
+          <img
+            src="/icons/approve.svg"
+            alt="approval check mark"
+          />
+          <p>Approve</p>
+        </button>
 
-      <div>
-        <button type="button" onClick={() => onApprove(content._id)}>Approve</button>
-        <a href={`/form/${content._id}`}>Edit</a>
-        <button type="button" onClick={() => onReject(content._id)}>Reject</button>
+        <a className={styles.edit} href={`/form/${content._id}`}>
+          <img
+            src="/icons/edit.svg"
+            alt="edit highlighter"
+          />
+          <p>Edit</p>
+        </a>
+
+        <button
+          className={styles.reject}
+          type="button"
+          onClick={() => onReject(content._id)}
+        >
+          <img
+            src="/icons/reject.svg"
+            alt="rejection cross"
+          />
+          <p>Reject</p>
+        </button>
       </div>
     </div>
   );
