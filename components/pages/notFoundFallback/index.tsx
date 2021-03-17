e92012c6fa9bc3bc7413e4c5495577135801e603
Adam McQuilkin <46639306+ajmcquilkin@users.noreply.github.com>
2021-03-17 15:40:21 -0400

    Fixed 404 page styling

--- components/pages/notFoundFallback/index.tsx
+++ components/pages/notFoundFallback/index.tsx
@@ -1,18 +1,18 @@
 import Link from 'next/link';
 import { Helmet } from 'react-helmet';
+
 import { siteMetaTitle } from 'utils';
 import styles from './notFoundFallback.module.scss';
 
 const NotFoundFallback = (): JSX.Element => (
-  <div>
+  <div className={styles.notFoundFallbackContainer}>
     <Helmet>
       <title>{`404 - ${siteMetaTitle}`}</title>
     </Helmet>
-    <div className={styles.notFoundFallbackContainer}>
-      <h1>404: Page not Found</h1>
-      <p>The page you&apos;re looking for no longer exists.</p>
-      <Link href="/"><a>Home</a></Link>
-    </div>
+
+    <h1>404: Page not Found</h1>
+    <p>The page you&apos;re looking for no longer exists.</p>
+    <Link href="/"><a>Home</a></Link>
   </div>
 );
 
